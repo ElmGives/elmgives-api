fa1c09a2cfc9a1beef65f0a31496937206d2b4a7
lesterzone <lesterzone@gmail.com>
2016-03-29 11:39:09 -0600

    Unit test for Post model
        validate fields
        types
        required fields

---
+++ tests/unit/models/post.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const tape = require('tape');
+const Post = require('../../../posts/post');
+const types = require('../types');
+const required = require('../required');
+
+tape('Post model', test => {
+    test.plan(10);
+
+    let post = new Post({});
+    let values = post.schema.paths;
+
+    types(['textContent'], values, test, 'String');
+    types(['createdAt', 'updatedAt'], values, test, 'Date');
+    types(['userId', 'npoId'], values, test, 'ObjectID');
+    types(['videos', 'images'], values, test, 'Array');
+
+    post.validate(error => {
+        required(['userId', 'npoId', 'textContent'], error.errors, test);
+    });
+
+    new Post({
+        userId: 'x'.repeat(24),
+        npoId: 'x'.repeat(24),
+        textContent: 'foobar'
+    }).validate(error => test.equal(undefined, error, 'valid with attributes'));
+});
