3378e130ce8eae7c8d445a149f6dd124c4481978
lesterzone <lesterzone@gmail.com>
2016-04-04 10:16:38 -0600

    Log proper query to be used on mongo console
        allow logs to be used on  mongo console, by changing the format of
        mongoose queries

--- helpers/querySerializer.js
+++ helpers/querySerializer.js
@@ -5,7 +5,7 @@
 'use strict';
 
 module.exports = (data) => {
-    let query = JSON.stringify(data.query);
+    let query = JSON.stringify(data.query).replace(/\"/g, '');
     let options = JSON.stringify(data.options || {});
 
     return `db.${data.coll}.${data.method}(${query}, ${options});`;
