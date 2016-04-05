27fb59c42b160df0c1117552db7a2f1258944e41
lesterzone <lesterzone@gmail.com>
2016-04-05 13:52:52 -0600

    Upload images to specific folder on s3
        allow admin users to upload images and choose folder destiny

--- helpers/querySerializer.js
+++ helpers/querySerializer.js
@@ -5,7 +5,7 @@
 'use strict';
 
 module.exports = (data) => {
-    let query = JSON.stringify(data.query).replace(/\"/g, '');
+    let query = JSON.stringify(data.query);
     let options = JSON.stringify(data.options || {});
 
     return `db.${data.coll}.${data.method}(${query}, ${options});`;
--- images/upload.js
+++ images/upload.js
@@ -12,18 +12,33 @@ let s3 = new AWS.S3({
 });
 
 /**
+ * Submit form input should be named as defined on this availableFolders
+ * That's our way to upload content to specific folder on S3
+ */
+let availableFolders = {
+    posts: 'posts',
+    logos: 'logos'
+};
+
+/**
  * image = formidable 'part'
  */
 module.exports = function upload(image) {
     let object = {
         Bucket: process.env.AWS_S3_BUCKET,
-        Key: image.filename
-            // object.ContentType = image.headers['content-type'];
+        Key: `${image.name}/${image.filename}`,
+        ACL: 'public-read'
     };
 
     return new Promise((resolve, reject) => {
+
+        if (!image.name || !availableFolders[image.name]) {
+            let error = new Error();
+            error.message = 'Field name is required';
+            return reject(error);
+        }
+
         s3.putObject(object, (error, data) => {
-            console.log('on put content', error, data);
             return error ? reject(error) : resolve(data);
         });
     });
