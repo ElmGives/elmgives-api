e860637e7a5d2f2767803ef613ea325a360f33a1
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-04-13 23:15:29 -0500

    Added tests for Transaction model and endpoints

---
+++ tests/unit/routes/transactions.test.js
@@ -0,0 +1,45 @@
+/**
+ * Test Transaction routes
+ */
+
+'use strict';
+
+const tape = require('tape');
+const transactions = require('../../../transactions/chain');
+
+let data = {};
+let middlewares = {};
+
+let options = {
+    'get /transactions': 'get /transactions'
+};
+
+transactions.stack.map(item => {
+    let method = Object.keys(item.route.methods).join('');
+    let path = item.route.path;
+    let key = `${method} ${path}`;
+    data[key] = key;
+
+    item.route.stack.map(item => {
+        middlewares[key] = middlewares[key] || [];
+        middlewares[key].push(item.name);
+    });
+});
+
+tape.test('Transaction Endpoints', test => {
+    test.plan(1);
+
+    Object.keys(options).map(key => {
+        test.equal(key, data[key], `should provide ${key} endpoint`);
+    });
+});
+
+tape.test('Transaction endpoints middlewares', test => {
+    test.plan(3);
+    Object.keys(middlewares).map(key => {
+        let actual = middlewares[key];
+        test.equal('verifyToken', actual[0], `should validate token on ${key}`);
+        test.equal('authenticate', actual[1], `should authenticate on ${key}`);
+        test.equal('currentUser', actual[2], `verify current user on ${key}`);
+    });
+});
