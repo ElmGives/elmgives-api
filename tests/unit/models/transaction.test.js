03339e3fbc7f0620c741ff4b8083955af5d7acfe
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-04-14 21:59:01 -0500

    Changed indentation to 4 spaces

--- tests/unit/models/transaction.test.js
+++ tests/unit/models/transaction.test.js
@@ -16,32 +16,32 @@ stringFields.push('hash.type');
 stringFields.push('hash.value');
 
 tape('Transaction model', test => {
-  test.plan(1 + requiredFields.length + numberFields.length + stringFields.length);
+    test.plan(1 + requiredFields.length + numberFields.length + stringFields.length);
 
-  let transaction = new Transaction({});
-  let values = transaction.schema.paths;
+    let transaction = new Transaction({});
+    let values = transaction.schema.paths;
 
-  types(numberFields, values, test, 'Number');
-  types(stringFields, values, test, 'String');
+    types(numberFields, values, test, 'Number');
+    types(stringFields, values, test, 'String');
 
-  transaction.validate(error => {
-    required(requiredFields, error.errors, test);
-  });
+    transaction.validate(error => {
+        required(requiredFields, error.errors, test);
+    });
 
-  new Transaction({
-    hash: {type: 'sha256', value: '1bd28a7ec31c9dae34c8ea7f6e138df19be66d2e843fe51082e6eabaac7ae37c'},
-      payload: {
-        count: 0,
-        address: 'wYAAm2TQ8ToXJAUVQFrmZQJR3pnBYcwxuL',
-        amount: 0,
-        roundup: 0,
-        balance: 0,
-        currency: 'USD',
-        limit: -10,
-        previous: null,
-        timestamp: 'Sat Apr 09 2016 08:14:07 GMT-0500 (COT)',
-        reference: 'plaid_transaction_id'
-      },
-    signatures: []
-  }).validate(error => test.equal(undefined, error, 'valid with attributes'));
+    new Transaction({
+        hash: {type: 'sha256', value: '1bd28a7ec31c9dae34c8ea7f6e138df19be66d2e843fe51082e6eabaac7ae37c'},
+            payload: {
+                count: 0,
+                address: 'wYAAm2TQ8ToXJAUVQFrmZQJR3pnBYcwxuL',
+                amount: 0,
+                roundup: 0,
+                balance: 0,
+                currency: 'USD',
+                limit: -10,
+                previous: null,
+                timestamp: 'Sat Apr 09 2016 08:14:07 GMT-0500 (COT)',
+                reference: 'plaid_transaction_id'
+            },
+        signatures: []
+    }).validate(error => test.equal(undefined, error, 'valid with attributes'));
 });
--- transactions/chain/list.js
+++ transactions/chain/list.js
@@ -6,40 +6,40 @@
 const Transaction = require('./transaction');
 
 module.exports = function list(request, response, next) {
-  let user = request.currentUser;
-  let npo = request.query.npo;
-  let address;
+    let user = request.currentUser;
+    let npo = request.query.npo;
+    let address;
 
-  try {
-    address = user.wallet.addresses[npo][0];
-  } catch(e) {
-    let error = new Error();
-    error.status = 404;
-    error.message = npo ? `No address found for NPO ${npo}`: 'Please provide the NPO name';
-    return next(error);
-  }
-  if (typeof address !== 'string') {
-    let error = new Error();
-    error.status = 404;
-    error.message = `No address found for NPO ${npo}`;
-    return next(error);
-  }
+    try {
+        address = user.wallet.addresses[npo][0];
+    } catch(e) {
+        let error = new Error();
+        error.status = 404;
+        error.message = npo ? `No address found for NPO ${npo}`: 'Please provide the NPO name';
+        return next(error);
+    }
+    if (typeof address !== 'string') {
+        let error = new Error();
+        error.status = 404;
+        error.message = `No address found for NPO ${npo}`;
+        return next(error);
+    }
 
-  let query = {'payload.address': address};
+    let query = {'payload.address': address};
 
-  return Transaction
-    .find(query)
-    .then(transactions => {
-      let data = transactions.map(transaction => {
-        return transaction.payload;
-      });
+    return Transaction
+        .find(query)
+        .then(transactions => {
+            let data = transactions.map(transaction => {
+                return transaction.payload;
+            });
 
-      return response.json({
-        data: data,
-        metadata: {
-          count: transactions.length
-        }
-      });
-    })
-    .catch(next);
+            return response.json({
+                data: data,
+                metadata: {
+                    count: transactions.length
+                }
+            });
+        })
+        .catch(next);
 };
