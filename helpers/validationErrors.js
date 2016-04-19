8487f205076ed0b3bb46b9f2c5f1e8c55ac010fe
lesterzone <lesterzone@gmail.com>
2016-04-19 13:29:40 -0600

    Validation error handler for Mandrill emails
        send proper errors when handling email errors

--- helpers/validationErrors.js
+++ helpers/validationErrors.js
@@ -4,7 +4,7 @@
 'use strict';
 
 module.exports = (error) => {
-    let errors = error.errors;
+    let errors = error.errors || {};
     let data = {};
     Object.keys(errors).map(field => data[field] = errors[field].message);
     return data;
