2a383b46460fc79f960ed14e516a4374ae055e1f
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-04-24 10:09:12 -0500

    Added tests for Address model

--- addresses/address.js
+++ addresses/address.js
@@ -11,7 +11,7 @@ let schema = new mongoose.Schema({
         required: true,
     },
     keys: {
-        schema: {
+        scheme: {
             type: String,
             default: 'ed25519',
         },
