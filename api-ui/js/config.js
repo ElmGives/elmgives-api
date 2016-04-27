156cd50108d99e5513c9210e871e926e6049d453
lesterzone <lesterzone@gmail.com>
2016-04-27 10:57:34 -0600

    Request single user API UI
        Allow admins to get single user information

--- api-ui/js/config.js
+++ api-ui/js/config.js
@@ -13,6 +13,16 @@ var users = {
             'url': API + '/users',
             'method': 'GET'
         },
+        'single user': {
+            'description': 'Single user information.',
+            'params': {
+                'auth_token': 'string',
+                'id': 'string'
+            },
+            'url': API + '/users/[id]',
+            'urlWithId': 'id',
+            'method': 'GET'
+        },
         'create new user': {
             'description': 'Posts a user.',
             'params': {
