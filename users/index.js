94b0a19e21cca61b83c2871f6516a811023fd830
lesterzone <lesterzone@gmail.com>
2016-04-28 13:37:58 -0600

    Allow admin to create account https://elmgives.atlassian.net/browse/ELM-24 Allow admin/user to create account

--- users/index.js
+++ users/index.js
@@ -5,6 +5,7 @@
 'use strict';
 
 const router = require('express').Router();
+const customMiddlewares = require('../lib/customMiddlewares');
 const verifyToken = require('../lib/verifyJwt');
 const authenticate = require('../lib/authenticate');
 const currentUser = require('../lib/currentUser');
@@ -16,9 +17,23 @@ const show = require('./show');
 const PATH = '/users';
 const SINGLE = '/users/:id';
 
+const middlewares = [
+    verifyToken, authenticate, currentUser, isAdmin, create
+];
+
+function adminOrUser(request, response, next) {
+    const token = request.headers.authorization;
+
+    if (token) {
+        return customMiddlewares(middlewares, request, response, next);
+    }
+
+    return customMiddlewares([create], request, response, next);
+}
+
 router
     .get(SINGLE, verifyToken, authenticate, currentUser, isAdmin, show)
     .get(PATH, verifyToken, authenticate, currentUser, isAdmin, list)
-    .post(PATH, create);
+    .post(PATH, adminOrUser);
 
 module.exports = router;
