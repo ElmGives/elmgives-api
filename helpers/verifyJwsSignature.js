73fb3809d928344e23b6bb99d43fe7e4223b07b8
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-05-12 11:03:00 -0500

    - Added helper for retrieving verified balances - Added deriveWalletAddress helper - Improved verifyJwsSignature helper - Removed 'statement' from seed transactions - Updated one latestTransaction signature - Added bs58check and ripemd160 dependencies

--- helpers/verifyJwsSignature.js
+++ helpers/verifyJwsSignature.js
@@ -4,18 +4,39 @@
 'use strict';
 
 const logger = require('../logger');
+const stringify = require('json-stable-stringify');
+const crypto = require('crypto');
+const elliptic = require('elliptic');
+const schemes = {
+    ed25519: new elliptic.ec('ed25519')
+};
 
-module.exports = function verifyJwsSignature(jws, schema, publicKey) {
-    let hash      = jws.hash.value;
+module.exports = function verifyJwsSignature(jws, scheme, publicKey) {
+    let hashValue = jws.hash.value;
     let signature = jws.signatures[0].signature;
     let verified  = null;
+    scheme        = scheme || schemes.ed25519;
 
-    try {
-        verified = schema.verify(hash, signature, publicKey, 'hex');
-    } catch(error) {
-        logger.error({ err: error });
-        return Promise.reject(new Error('invalid signature'));
+    // Preventive, reusable error object
+    let error = new Error();
+    error.status = 422;
+    error.message = 'invalid-transaction-signature';
+    error.details = `transaction hash: ${hashValue}`;
+
+    let hash = crypto.createHash(jws.hash.type || 'sha256')
+        .update(stringify(jws.payload)).digest('hex');
+    if (hash !== hashValue) {
+        logger.error(error);
+        error.message = 'transaction-hash-mismatch';
+        return Promise.reject(error);
     }
 
-    return Promise.resolve(verified);
+    try {
+        verified = scheme.verify(hash, signature, publicKey, 'hex');
+    } catch(err) {
+        logger.error(error);
+        return Promise.reject(error);
+    }
+
+    return verified ? Promise.resolve(true) : Promise.reject(error);
 };
