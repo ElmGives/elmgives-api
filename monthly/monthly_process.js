3d5937f9761fb4beb4a7935bf27873f28f9efbe4
hectorfhurtado <hurtado.fernando@gmail.com>
2016-05-16 10:27:55 -0500

    First implementation and inline documentation

--- monthly/monthly_process.js
+++ monthly/monthly_process.js
@@ -1,6 +1,5 @@
 /**
  * STEPS:
- * - Get NPOs, by [['stripe.accountId']] and [[active]]
  * - Get Users with [[stripe]] attribute and a value different from default
  * - Filter out those users with [[stripe.token]] attribute
  *   - If they have a [[CustomerId]] attibute, then delete the token
@@ -15,8 +14,8 @@
  *       - Clamp donation if needed
  *   - If everything is good, make donation (remember donations are in cents)
  *   - Request a new [[address]]
- *   - Save new [[address]] on User NPO 
- *   - Create a new [[address]] on Addresses. This adds a previous transaction with zero value
+ *   - Save new [[address]] on User NPO (this is done by pledge manager)
+ *   - Create a new [[address]] on Addresses. This adds a previous transaction with zero value (this is done by pledge manager)
  * 
  * On any fail log error
  */
@@ -27,6 +26,11 @@ const Users = require('../users/user');
 const logger = require('../logger');
 const getAddress = require('../addresses/read');
 const getTransaction = require('../transactions/read');
+const notify = require('../slack/index');
+
+const aws = require('../lib/awsQueue');
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // GLOBAL VARIABLES
 let processGen = null;
@@ -35,25 +39,35 @@ let processGen = null;
  * Starts the process
  */
 function run() {
-  
   processGen = executeProcess();
   processGen.next();
 }
 
 /**
- * List every step from above, It 'waits' for creation of a Stripe customer when he doesn't exist
+ * Every step in this process is documented inline
  */
 function *executeProcess() {
+  let users = [];
+  let user = null;
   
+  // We retrieve every person that has stripe and plaid attributes and are active
   try {
-    let npos = yield getNpos(processGen);
-    let users = yield getUsers(processGen);
-    let user = null;
+    users = yield getUsers(processGen);
+  } catch( error ) {
+    logger.error({ err: error });
+    users = [];
+  }
     
-    // Used a while instead of a loop in case we end up adding users asyncronously
-    // if user database is too big to process in one swoop
-    while((user = users.pop())) {
+  // Used a while instead of a loop in case we end up adding users asyncronously
+  // if user database is too big to process in one swoop
+  while((user = users.pop())) {
+    
+    try {
       
+      // If the user has a stripe token, we check if (s)he has a stripe customerId
+      // If a customerId is present, we remove the token because it's useless, if not, we
+      // immediately create a stripe account because with the token in this state can do a lot and
+      // is not safe for our user to delay that creation 
       if (user.stripe.token) {
         
         if (user.stripe.customerId) {
@@ -69,17 +83,65 @@ function *executeProcess() {
         }        
       }
       
+      // Now we look for the latestTransaction which contains the balance to charge.
+      // We verify it not charged yet. If it is, maybe something went wrong on the round up process or
+      // user simply didn't make any use of registered account
       let address = user.pledges[0].addresses[0];
       let latestTransactionHash = yield getLatestTransactionHash(address, processGen);
       let latestTransaction = yield getLatestTransaction(latestTransactionHash, processGen);
       
-      // TODO: continue with - If this [[transaction]] is already processed, notify?
+      if (latestTransaction.charged) {
+        notify({ text: `This transaction was already processed: ${latestTransaction.hash.value}` });
+        
+        let error = new Error(`This transaction was already processed: ${latestTransaction.hash.value}`);
+        throw error;
+      }
+      
+      // We verify that transaction hash is not tampered. Also that the address is derived from public key
+      // we get the address, amount to charge and currency
+      let verifiedData = yield verifyData(address, processGen);
+      
+      if (!verifiedData) {
+        let error = new Error('Transaction integrity or signature don\'t match');
+        throw error;
+      }
+      
+      // we verify and clamp if necessary user monthlyLimit
+      let monthlyLimit = user.pledges[0].monthlyLimit;
+      
+      if (verifyData.balance > monthlyLimit) {
+        verifiedData.balance = monthlyLimit;
+      }
+      
+      // every charge is in cents and get user pledge npo object to get access to stripe connect account
+      let cents = verifiedData.balance * 100;
+      let npo = yield getNpo(user.pledges[0].npo, processGen);
+      
+      if (!npo) {
+        let error = new Error(`User ${user._id}, doesn't have a valid pledge name`);
+        throw error;
+      }
+      
+      // then we try to make the donation
+      let donationSuccess = yield makeDonation(cents, verifiedData.currency, user.stripe.customerId, npo.stripe.accountId, processGen);
+      
+      if (!donationSuccess) {
+        let error = new Error(`Donation couldn't be made for user ${user._id}`);
+        throw error;
+      }
+      
+      // Once donated, we need to request a new address for user pledge to start a new month
+      const pledgeId = user.pledges[0]._id;
+      let newAddress = yield createNewAddress(user._id, pledgeId, monthlyLimit, processGen);
+      
+      if (!newAddress) {
+        let error = new Error(`Couldn't send AWS request for new Address for user ${user._id}`);
+        throw error;
+      }
+    } catch(error) {
+      logger.error({ err: error });
     }
-  } catch( error ) {
-    logger.error({ err: error });
   }
-  
-  
 } 
 
 /**
@@ -88,8 +150,9 @@ function *executeProcess() {
  * that would make the code difficult to read. When using nodejs v6+ this can be achieved using destructuring
  * @param {generator} generator
  */
-function getNpos(generator) {
+function getNpo(npoName, generator) {
   const query = {
+    name: npoName,
     active: true,
     'stripe.accountId': {
       $exists: true,
@@ -98,12 +161,13 @@ function getNpos(generator) {
     
   const selector = {
     stripe: 1,
+    name: 1,
   };
     
   Npos
-    .find(query, selector)
+    .findOne(query, selector)
     .exec()                     // this forces to return a real Promise
-    .then( npos => generator.next(npos))
+    .then( npo => generator.next(npo))
     .catch(error => generator.throw(error));
 }
 
@@ -125,6 +189,7 @@ function getUsers(generator) {
   
   const selector = {
     stripe: 1,
+    pledges: 1,
   };
   
   Users
@@ -165,8 +230,8 @@ function removeStripeToken(user, generator) {
  */
 function createNewCustomer(user, generator) {
   
-  // TODO: find out if this helper function is already on develop
-  generator.next(1);
+  // Implemented in https://github.com/ElmGives/elmgives-api/pull/41 not yet merged
+  generator.next(true);
 }
 
 /**
@@ -234,6 +299,56 @@ function getLatestTransaction(hash, generator) {
       
       generator.next(transaction);
     });
+}
+
+/**
+ * Verifies integrity of every transaction componenet is to be trusted
+ * @param {String}    address
+ * @param {generator} generator
+ */
+function verifyData(address, generator) {
+  // Implemented in https://github.com/ElmGives/elmgives-api/pull/42 but not merged yet
+  generator.next(true);
+}
+
+/**
+ * Uses Stripe API to make a donation on behalf on [[connectedStripeAccount]]
+ * @param {Number}    amount
+ * @param {String}    currency  'usd' for now
+ * @param {String}    connectedStripeAccount
+ * @param {generator} generator
+ */
+function makeDonation(amount, currency, customer, connectedStripeAccount, generator) {
+
+  stripe.charges.create({
+    amount: amount,
+    currency: currency,
+    customer: customer,
+    destination: connectedStripeAccount,
+  }).then(() => generator.next(true))
+  .catch(error => generator.throw(error));
+}
+
+/**
+ * Address manager is in charge to add a new Address on Addresses collection and to update
+ * User pledges address which is signaled with the sendMessage function
+ * @param {String}    userId
+ * @param {String}    pledgeId
+ * @param {Number}    monthlyLimit
+ * @param {generator} generator
+ */
+function createNewAddress(userId, pledgeId, monthlyLimit, generator) {
+  
+  aws.sendMessage({
+    userId: userId,
+    pledgeId: String(pledgeId),
+    limit: monthlyLimit,
+    nonce: String((new Date()).getTime())
+  }, {
+    queue: process.env.AWS_SQS_URL_ADDRESS_REQUESTS
+  })
+  .then(() => generator.next(true))
+  .catch(error => generator.throw(error));
 }
 
 module.exports = {
