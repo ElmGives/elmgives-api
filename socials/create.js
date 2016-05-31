ecd9ba96345e847c2cee3e6641dfb31041d83c1e
lesterzone <lesterzone@gmail.com>
2016-05-31 09:40:16 -0600

    Create social after validate provider information
    Create Social instance and store user information on social login

--- socials/create.js
+++ socials/create.js
@@ -4,10 +4,30 @@
 'use strict';
 
 const Social = require('/social');
+const validateFacebook = require('./validateFacebook');
 
-module.exports = (request, response) => {
-    return new Social(request.body)
-        .save()
+module.exports = function create(request, response, next) {
+    let token = request.body.token;
+    let email = request.body.email;
+    let providerId = request.body.providerId;
+
+    if (!token && !providerId && !email) {
+        let error = new Error();
+        error.status = 422;
+        error.message = 'Required fields missing: email, token, providerId';
+
+        return next(error);
+    }
+
+    return validateFacebook(token)
+        .then(() => new Social(request.body).save())
+        .then(saved => {
+            /**
+             * TODO:
+             * Create session and return it.
+             */
+            return saved;
+        })
         .then(data => response.json(data))
-        .catch(error => response.status(422).json(error));
+        .catch(next);
 };
