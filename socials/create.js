ce557509685cec3fb74c81f2a5ef787f09d5d991
lesterzone <lesterzone@gmail.com>
2016-06-15 10:12:38 -0600

    Create user and session from social login
    Allow user to login with social media: Facebook only

--- socials/create.js
+++ socials/create.js
@@ -1,9 +1,20 @@
 /**
  * Middleware to create socials based on request body object provided
+ *
+ * 1.  Validate token against facebook API
+ * 2.  If provided token is invalid, return error, otherwise, find Social with
+ *     provided providerId and email
+ * 3.  If social found, create session and return , otherwise, create social,
+ * 4.  Create session and return
  */
 'use strict';
 
 const Social = require('/social');
+const User = require('../users/user');
+const Session = require('../sessions/session');
+const uniqueToken = require('../helpers/token');
+const expire = require('../helpers/expire');
+const EXPIRE = process.env.EXPIRE_HOURS;
 const validateFacebook = require('./validateFacebook');
 
 module.exports = function create(request, response, next) {
@@ -20,13 +31,61 @@ module.exports = function create(request, response, next) {
     }
 
     return validateFacebook(token)
-        .then(() => new Social(request.body).save())
-        .then(saved => {
+        .then(valid => {
+            if (!valid) {
+                let error = Error();
+                error.message = 'invalid token';
+                error.status = 422;
+
+                return Promise.reject(error);
+            }
+
+            let query = {
+                providerId: providerId,
+                email: email
+            };
+
+            return Social.findOne(query);
+        })
+        .then(social => {
+            if (!social) {
+                // create social, and user and return
+                // user data needed to create an account: email and password
+                // Since password is not present on social login, @cooper said
+                // let's use a random
+                let userData = {
+                    email: email,
+                    password: uniqueToken()
+                };
+
+                new User(userData).save();
+                return new Social(request.body).save();
+            }
+
+            return social;
+        })
+        .then(social => {
+            let query = {
+                email: social.email,
+            };
             /**
-             * TODO:
-             * Create session and return it.
+             * If request get's here, means:
+             *     user's token is a valid Facebook token, social information
+             *     exist and we are ready to find related user and create a
+             *     session.
              */
-            return saved;
+            return User.findOne(query);
+        })
+        .then(user => {
+            let session = {
+                token: uniqueToken(),
+                expire: expire(EXPIRE),
+                userId: user._id,
+                agent: request.headers['user-agent'],
+                verified: true
+            };
+
+            return new Session(session).save();
         })
         .then(data => response.json(data))
         .catch(next);
