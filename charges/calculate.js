ebaabd40affc26f954a9fbba48e8faa0e5d14bb9
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-08-11 10:25:30 -0500

    feat(charges): prepare pending charges - Added logic for building charges in pending state. - Added file for charge calculation (pending implementation). - Added 'moment' dependency.
    https://elmgives.atlassian.net/browse/ELM-282

---
+++ charges/calculate.js
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+'use strict';
+
+module.exports = function calculateCharge() {
+    let charge = {
+        amount: 0,
+        currency: 'usd',
+        fee: 0,
+        net: 0
+    };
+
+    return charge;
+};
