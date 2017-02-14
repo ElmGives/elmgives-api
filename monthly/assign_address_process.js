0a1ca4664f9972adf4f3e80f081c860571319215
Jorge Zaccaro <jorgezaccaro@gmail.com>
2017-02-13 19:51:20 -0500

    feat(addresses): add assignment date option

--- monthly/assign_address_process.js
+++ monthly/assign_address_process.js
@@ -17,31 +17,33 @@ require('../config/database');
 
 const logger = require('../logger');
 const getUsers = require('./getUsers');
-const getYearMonth = require('../helpers/getYearMonth');
+// const getYearMonth = require('../helpers/getYearMonth');
 const createNewAddress = require('./createNewAddress');
 const updateLastRun = require('../runs/update');
 const notify = require('../slack/index');
+const moment = require('moment');
 
 // GLOBAL VARIABLES
 let addressGen = null;
 const hardCodedMonthlyLimit = 5000;
+const YM = 'YYYY-MM';
 
 /**
  * Assign new address process
  * Starts the executeAddressAssign generator
  */
-function assignNewAddress() {
+function assignNewAddress(options) {
     logger.info('Monthly address assignment: started');
     notify('Address assignment process starts');
     
-    addressGen = executeAddressAssign();
+    addressGen = executeAddressAssign(options || {});
     addressGen.next();
 }
 
 /**
  * Every step is documented inline
  */
-function *executeAddressAssign() {
+function *executeAddressAssign(options) {
     let users = [];
     let user = null;
     
@@ -79,8 +81,8 @@ function *executeAddressAssign() {
         }
         
         // we check user doesn't have already an address for this month. If it exists, we skip the process
-        let date = new Date();
-        let thisMonth = getYearMonth(date);
+        let date = moment(options.date);
+        let thisMonth = date.format(YM);
         let existentAddress = activePledge.addresses[thisMonth];
         
         if (existentAddress) {
